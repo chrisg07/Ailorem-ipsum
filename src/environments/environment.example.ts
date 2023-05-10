21247877c9bf96f9cf17eca7b91e4fa0fe9a41aa
chrisg07 <chrisg07@vt.edu>
2023-05-09 22:04:40 -0500

    example environment file

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiKey: 'YOUR API KEY HERE',
+  prompts: {
+    topicIpsum: "You know what lorem ipsum is. I want you to generate something similar to lorem ipsum but infused with a specific topic. That topic is: ",
+    incentivizeHTML: `Please format the response into a randomly generated HTML structure using the following tags: <p>, and nothing else`
+  }
+};
