7f3a78cd63719cdbb09f220c60271ad5a1c66827
chrisg07 <chrisg07@vt.edu>
2023-05-12 08:56:06 -0500

    now calling firebase function to protext api key

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,40 +1,42 @@
-import * as functions from "firebase-functions";
-import { Configuration, OpenAIApi } from "openai";
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const axios = require('axios');
+const cors = require('cors')({ origin: true });
+admin.initializeApp();
 
-import * as cors from 'cors';
+exports.openAiResponse2 = functions.https.onRequest(async (request: any, response: any) => {
+  const apiKey = process.env.OPENAI_API_KEY;
+  const topic = request.query.topic;
+  cors(request, response, async () => {
+    try {
+      const apiResponse = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [
+            {
+              role: 'user',
+              content: 'Generate gibberish like lorem ipsum but about a certain topic. That topic is: ' + topic
+            }
+          ],
+          temperature: 0.7,
+          max_tokens: 200
+        },
+        {
+          headers: {
+            'Authorization': `Bearer ${apiKey}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
 
-const corsHandler = cors({ origin: true });
-
-
-// const DEFAULT_MODEL = "text-davinci-003";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-// eslint-disable-next-line max-len
-export const openAIHttpFunction = functions.https.onRequest(async (request, response) => {
-  corsHandler(request, response, async () => {
-    const params = request.query;
-
-    if (!params.query) {
-      response.send("Please Send Query Parameter");
-    }
-
-    const openAPIResponse = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Generate lorem ipsum about little debbie snacks",
-      max_tokens: 200,
-      temperature: 7,
-    }).catch(error => {
-      response.status(400).send().json({
-        status: 'error',
-        message: error.message
+      response.status(200).json({
+        message: 'OpenAI API connection successful.',
+        completion: apiResponse.data.choices[0].message.content,
       });
-
-    response.send(JSON.stringify(openAPIResponse.data));
+    } catch (error) {
+      console.error('Error connecting to OpenAI API:', error);
+      response.status(500).send('Error connecting to OpenAI API');
+    }
   });
-
 });
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import {environment} from "../../../environments/environment.development";
 import {Configuration, OpenAIApi} from "openai";
 import {map, Observable} from "rxjs";
 import {fromPromise} from "rxjs/internal/observable/innerFrom";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -15,29 +16,36 @@ export class HomeComponent {
   public topic!: any;
   public inHtml!: boolean;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   private getResponse(topic: string): void {
-    const configuration = new Configuration({
-      apiKey: environment.apiKey,
-    });
-    const openai = new OpenAIApi(configuration);
-    const response = openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: this.getPrompt(topic),
-      temperature: 0,
-      max_tokens: 1000,
-    });
-    fromPromise(response).pipe(
-      map(value => value)
-    ).subscribe((value) => {
-      // this.response = value;
-      console.log(value.data.choices[0])
-      const answer = value.data.choices[0].text;
-      if (answer) {
-        this.response = answer;
-      }
-    });
+    // const configuration = new Configuration({
+    //   apiKey: environment.apiKey,
+    // });
+    // const openai = new OpenAIApi(configuration);
+    // const response = openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: this.getPrompt(topic),
+    //   temperature: 0,
+    //   max_tokens: 1000,
+    // });
+    // fromPromise(response).pipe(
+    //   map(value => value)
+    // ).subscribe((value) => {
+    //   // this.response = value;
+    //   console.log(value.data.choices[0])
+    //   const answer = value.data.choices[0].text;
+    //   if (answer) {
+    //     this.response = answer;
+    //   }
+    // });
+
+    const url = 'https://us-central1-ailorem-ipsum.cloudfunctions.net/openAiResponse2?topic=';
+    this.http.get(url + this.topic)
+      .subscribe((res: any) => {
+        this.response = res.completion;
+        console.log(res)
+      })
   }
 
   public getPrompt(topic: string) {
