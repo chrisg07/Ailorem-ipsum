360f1949f104849ff9ed2630cc695340c67c9edc
Christopher Guilliams <chrisg07@vt.edu>
2024-02-28 22:03:00 -0600

    installed daisy ui library

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#795548",
+          "secondary": "8D6E63",
+          "accent": "#BCAAA4",
+          "neutral": "#E0E0E0",
+          "base-100": "#e5e7eb",
+          "info": "#4E342E",
+          "success": "#33691E",
+          "warning": "#FF6F00",
+          "error": "#B71C1C",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
