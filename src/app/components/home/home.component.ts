9beeafd109906c04b67b534985d8f8ac702216e5
Christopher Guilliams <chrisg07@vt.edu>
2024-02-28 23:26:02 -0600

    click to copy functionality

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -44,6 +44,20 @@ export class HomeComponent {
     }
   }
 
+  public copyResponse(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
   public clearSearch(): void {
     const topicsInput = document.getElementById("topics") as HTMLInputElement;
     topicsInput.value = "";
