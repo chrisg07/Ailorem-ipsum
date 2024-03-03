0bf4ef3786c85e2f08fbb7dee9393c5eaadb9435
Christopher Guilliams <chrisg07@vt.edu>
2024-03-03 18:53:57 +0000

    configured Prettier

--- src/app/components/copy-button/copy-button.component.ts
+++ src/app/components/copy-button/copy-button.component.ts
@@ -1,15 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { CopyToClipboardService } from 'src/app/services/copy-to-clipboard.service';
+import { Component, Input } from "@angular/core";
+import { CopyToClipboardService } from "src/app/services/copy-to-clipboard.service";
 
 @Component({
-  selector: 'app-copy-button',
-  templateUrl: './copy-button.component.html',
-  styleUrls: ['./copy-button.component.scss']
+	selector: "app-copy-button",
+	templateUrl: "./copy-button.component.html",
+	styleUrls: ["./copy-button.component.scss"]
 })
 export class CopyButtonComponent {
-  @Input() value: string = '';
-  
-  constructor(public copyToClipboard: CopyToClipboardService) {
-    
-  }
+	@Input() value: string = "";
+
+	constructor(public copyToClipboard: CopyToClipboardService) {}
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,35 +1,41 @@
-import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import { Observable, from } from 'rxjs';
-import { FirebaseService, GeneratedResponse } from 'src/app/services/firebase.service';
-import { CopyToClipboardService } from 'src/app/services/copy-to-clipboard.service';
-import { ResponseService } from 'src/app/services/response.service';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, from } from "rxjs";
+import {
+	FirebaseService,
+	GeneratedResponse
+} from "src/app/services/firebase.service";
+import { CopyToClipboardService } from "src/app/services/copy-to-clipboard.service";
+import { ResponseService } from "src/app/services/response.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: "app-home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent {
+	public responses: { content: string; topic: string }[] = [];
+	public topic!: any;
+	public inHtml!: boolean;
 
-  public responses: {content: string, topic: string}[] = [];
-  public topic!: any;
-  public inHtml!: boolean;
+	generatedResponses$: Observable<GeneratedResponse[]>;
 
-  generatedResponses$: Observable<GeneratedResponse[]>;
+	constructor(
+		private firebase: FirebaseService,
+		public copyToClipboard: CopyToClipboardService,
+		private response: ResponseService
+	) {
+		this.generatedResponses$ = from(this.firebase.getGeneratedResponses());
+	}
 
-  constructor(private firebase: FirebaseService, public copyToClipboard: CopyToClipboardService, private response: ResponseService) {
-    this.generatedResponses$ = from(this.firebase.getGeneratedResponses());
-  }
+	public onSubmit(): void {
+		this.response.getResponse(this.topic).subscribe(res => {
+			this.responses.push({ content: res.completion, topic: this.topic });
+		});
+	}
 
-  public onSubmit(): void {
-    this.response.getResponse(this.topic).subscribe(res => {
-      this.responses.push({content: res.completion, topic: this.topic})
-    });
-  }
-
-  public clearSearch(): void {
-    const topicsInput = document.getElementById("topics") as HTMLInputElement;
-    topicsInput.value = "";
-  }
+	public clearSearch(): void {
+		const topicsInput = document.getElementById("topics") as HTMLInputElement;
+		topicsInput.value = "";
+	}
 }
